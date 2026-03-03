42ecb7a438187b30b2cda088420d88fd3308a865
tai.hoang <sts.taihoang@gmail.com>
2026-03-03 14:44:02 +0700

    Bai tap Git

---
+++ tests/hello-world.js
@@ -0,0 +1,38 @@
+const { type } = require("node:os");
+
+let myName = "Check";
+let myStore = "PerroPerro";
+
+console.log(myName);
+console.log(myStore);
+
+myName = "This Out"
+
+console.log(myName); 
+
+const petShop = "Perro Perro";
+
+console.log(petShop);
+
+//petShop = "Number 1";
+
+console.log(petShop); 
+
+const age = 25; // Số nguyên
+const price = 19.99; // Số thực
+const infinity = Infinity; // Vô hạn
+const notANumber = NaN; // Không phải là số 
+
+console.log(age);
+console.log(price);
+console.log(infinity);
+console.log(notANumber); 
+
+const isActive = true; // Giá trị đúng
+const isComplete = false; // Giá trị sai 
+
+console.log(isActive);
+console.log(isComplete);
+
+console.log(typeof price);
+console.log(typeof isComplete);
